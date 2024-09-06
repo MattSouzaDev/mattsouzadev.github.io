6ba5424a92e6cedf28b2ef8d83af946dfcadb63c
MattSouzaDev <souzabusinessmatheus@gmail.com>
2024-09-06 16:17:16 -0400

    imgs added, nav bar, flip card to about me container on the right, hover aninmations to a paragraph

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-window.onscroll = function () {
-    scrollFunction()
-};
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        document.getElementById("h1-header").style.fontSize = "25px";
-    } else {
-        document.getElementById("h1-header").style.fontSize = "35px";
-    }
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
 }
+
+const goToTop = document.getElementById("go-to-top");
+const goToBottom = document.getElementById("go-to-bottom");
+
+goToBottom.addEventListener("click", () => {
+    goToTop.scrollIntoView({ behavior: "smooth", block: "end" });
+});
+goToTop.addEventListener("click", () => {
+    goToBottom.scrollIntoView({ behavior: "smooth", block: "start" });
+});
