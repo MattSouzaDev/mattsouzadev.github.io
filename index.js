411f5ad6115874a5ac6a1a3f251dd22c463c1484
MattSouzaDev <souzabusinessmatheus@gmail.com>
2024-09-06 23:02:29 -0400

    theme modifications

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").style.padding = "3px 3px";
+    document.getElementById("logo").style.width = "20%";
   } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("logo").style.width = "30%";
   }
 }
 
