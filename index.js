1c294549b9792bf447db0318decb8f82309a5fca
MattSouzaDev <souzabusinessmatheus@gmail.com>
2024-09-10 18:50:44 -0400

    -a

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-window.onload = () => {
 
-  window.onscroll = function () { scrollFunction() };
 
-  const item1 = document.getElementById("item1")
-  const item2 = document.getElementById("item2")
-  const item3 = document.getElementById("item3")
+window.onscroll = function () { scrollFunction() };
 
-  function scrollFunction() {
+const item1 = document.getElementById("item1")
+const item2 = document.getElementById("item2")
+const item3 = document.getElementById("item3")
+const logo = document.getElementById("logo")
 
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-      item1.classList.remove('nava')
-      item2.classList.remove('nava')
-      item3.classList.remove('nava')
-      item1.classList.add('navbarShrink')
-      item2.classList.add('navbarShrink')
-      item3.classList.add('navbarShrink')
-      document.getElementById("logo").style.width = "20%";
-    } else {
-      item1.classList.remove('navbarShrink')
-      item2.classList.remove('navbarShrink')
-      item3.classList.remove('navbarShrink')
-      item1.classList.add('nava')
-      item2.classList.add('nava')
-      item3.classList.add('nava')
+function scrollFunction() {
 
-      document.getElementById("logo").style.width = "40%";
-    }
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    item1.classList.remove('nava')
+    item2.classList.remove('nava')
+    item3.classList.remove('nava')
+    item1.classList.add('navbarShrink')
+    item2.classList.add('navbarShrink')
+    item3.classList.add('navbarShrink')
+    logo.classList.remove('logo')
+    logo.classList.add('logo1')
+    
+  } else {
+    item1.classList.remove('navbarShrink')
+    item2.classList.remove('navbarShrink')
+    item3.classList.remove('navbarShrink')
+    item1.classList.add('nava')
+    item2.classList.add('nava')
+    item3.classList.add('nava')
+    logo.classList.remove('logo1')
+    logo.classList.add('logo')
   }
 }
