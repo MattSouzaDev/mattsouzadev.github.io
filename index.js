1c676ca773db231bb996b88f7e90004550230092
MattSouzaDev <souzabusinessmatheus@gmail.com>
2024-09-10 16:48:09 -0400

    Style changes, and mobile resposability

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-window.onscroll = function () { scrollFunction() };
+window.onload = () => {
 
-const item1 = document.getElementById("item1")
-const item2 = document.getElementById("item2")
-const item3 = document.getElementById("item3")
+  window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
+  const item1 = document.getElementById("item1")
+  const item2 = document.getElementById("item2")
+  const item3 = document.getElementById("item3")
 
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    item1.classList.remove('nava')
-    item2.classList.remove('nava')
-    item3.classList.remove('nava')
-    item1.classList.add('navbarShrink')
-    item2.classList.add('navbarShrink')
-    item3.classList.add('navbarShrink')
-    
+  function scrollFunction() {
 
-    document.getElementById("logo").style.width = "20%";
-  } else {
-    item1.classList.remove('navbarShrink')
-    item2.classList.remove('navbarShrink')
-    item3.classList.remove('navbarShrink')
-    item1.classList.add('nava')
-    item2.classList.add('nava')
-    item3.classList.add('nava')
-    
-    document.getElementById("logo").style.width = "30%";
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+      item1.classList.remove('nava')
+      item2.classList.remove('nava')
+      item3.classList.remove('nava')
+      item1.classList.add('navbarShrink')
+      item2.classList.add('navbarShrink')
+      item3.classList.add('navbarShrink')
+      document.getElementById("logo").style.width = "20%";
+    } else {
+      item1.classList.remove('navbarShrink')
+      item2.classList.remove('navbarShrink')
+      item3.classList.remove('navbarShrink')
+      item1.classList.add('nava')
+      item2.classList.add('nava')
+      item3.classList.add('nava')
+
+      document.getElementById("logo").style.width = "40%";
+    }
   }
 }
